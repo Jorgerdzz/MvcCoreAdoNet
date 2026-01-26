027d0be490e787e92c018a28330699f9745be6cf
Jorgerdzz <jorgerodriguezalonso20@gmail.com>
2026-01-26 13:46:21 +0100

    Add project files.

---
+++ MvcCoreAdoNet/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcCoreAdoNet.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult VistaEscudoLaurel()
+        {
+            return View();
+        }
+
+        public IActionResult VistaEscudo()
+        {
+            return View();
+        }
+
+        public IActionResult VistaFrenteAtletico()
+        {
+            return View();
+        }
+
+    }
+}
